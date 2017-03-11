490102902a73a80ab5fab71afa6fb19daf8dd98c
Simon Kindström <simon.kindstrom@gmail.com>
2017-03-11 16:01:31 +0900

    Finished BF_AllocBuf
    Also refactored any shared code between BF_GetBuf
    and BF_AllocBuf into seperate functions

--- bf/bf.c
+++ bf/bf.c
@@ -39,6 +39,77 @@ void FL_Clean(BFpage *node) {
   }
 }
 
+/* Pops the first element and updates the head */
+BFpage *FL_Pop(BFpage **head) {
+  BFpage *page = *head;
+  *(head) = page->nextpage;
+
+  return page;
+}
+
+/* Local functions */
+
+void initialize_bpage_no_read(BFreq bq, BFpage *page) {
+  page->dirty = FALSE;
+  page->count = 1;
+  page->fd = bq.fd;
+  page->pagenum = bq.pagenum;
+  page->nextpage = NULL;
+  page->prevpage = NULL;
+}
+
+/* Returns -1 on read error */
+int initialize_bpage(BFreq bq, BFpage *page) {
+  initialize_bpage_no_read(bq, page);
+
+  /* Read page data from an offset. It assumes that pagenum is zero-indexed */
+  lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
+  return read(bq.unixfd, page->fpage.pagebuf, PAGE_SIZE);
+}
+
+void LRU_Push(BFpage **head, BFpage *new_node) {
+  /* Update LRU. The least recently used element is at the front */
+  new_node->nextpage = *head;
+  (*head)->prevpage = new_node;
+  *head = new_node;
+}
+
+/*
+ * This function does a bit more than the name suggests
+ * It handles everything regarding removing an element from the LRU,
+ * writing to disk, and removes it from the hash table
+ *
+ * Returns -1 on write error, 0 otherwise
+ * Writes the cleared BFpage to *ret_bpage
+ */
+int LRU_ClearLast(BFpage *lru_head, BFhash_entry **hash_table, BFreq bq, BFpage **ret_bpage) {
+  BFpage *bpage = lru_head;
+  /*
+   * Find the least recently used object (the tail)
+   * TODO: Keep a pointer to the tail to not have to go through the whole list
+   */
+  while (bpage->nextpage) {
+    bpage = bpage->nextpage;
+  }
+
+  /* Only have to write if the page is dirty */
+  if (bpage->dirty) {
+    lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
+    if (write(bq.unixfd, bpage->fpage.pagebuf, PAGE_SIZE) == -1) {
+      return BFE_INCOMPLETEWRITE;
+    }
+  }
+
+  HT_Remove(hash_table, bpage->fd, bpage->pagenum);
+
+  /* Remove from the end of LRU */
+  bpage->prevpage->nextpage = NULL;
+  bpage->prevpage = NULL;
+
+  *ret_bpage = bpage;
+  return 0;
+}
+
 /* Variables */
 BFpage *free_list_head;
 BFpage *lru_head;
@@ -54,20 +125,6 @@ void BF_Init(void) {
   hash_table = HT_Init(BF_HASH_TBL_SIZE);
 }
 
-/* Returns -1 on read error */
-int initialize_bpage(BFreq bq, BFpage *page) {
-  page->dirty = FALSE;
-  page->count = 1;
-  page->fd = bq.fd;
-  page->pagenum = bq.pagenum;
-  page->nextpage = NULL;
-  page->prevpage = NULL;
-
-  /* Read page data from an offset. It assumes that pagenum is zero-indexed */
-  lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
-  return read(bq.unixfd, page->fpage.pagebuf, PAGE_SIZE);
-}
-
 int BF_GetBuf(BFreq bq, PFpage **fpage) {
   BFpage *bpage;
 
@@ -75,65 +132,52 @@ int BF_GetBuf(BFreq bq, PFpage **fpage) {
   bpage = HT_Find(hash_table, bq.fd, bq.pagenum);
   if (bpage) {
     ++bpage->count;
-    *fpage = &bpage->fpage;
-  } else if (free_list_head) {
-    /* if there is available space in FreeList just use that */
-
-    /* Pop an element from the free_list */
-    bpage = free_list_head;
-    free_list_head = bpage->nextpage;
-
-    if (initialize_bpage(bq, bpage) == -1) {
-      return BFE_INCOMPLETEREAD;
-    }
-    /* Update LRU. The least recently used element is at the front */
-    bpage->nextpage = lru_head;
-    lru_head->prevpage = bpage;
-
-    HT_Add(hash_table, bpage);
   } else {
-    /*
-     * If it's not in the hash table and there's no space in the free list,
-     * We have to kick a element from the LRU. Also make sure to remove
-     * it from the hash table
-     */
-
-    bpage = lru_head;
-    /*
-     * Find the least recently used object (the tail)
-     * TODO: Keep a pointer to the tail to not have to go through the whole list
-     */
-    while (bpage->nextpage) {
-      bpage = bpage->nextpage;
-    }
-
-    /* Only have to write if the page is dirty */
-    if (bpage->dirty) {
-      lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
-      if (write(bq.unixfd, bpage->fpage.pagebuf, PAGE_SIZE) == -1) {
+    if (free_list_head) {
+      /* if there is available space in FreeList just use that */
+      bpage = FL_Pop(&free_list_head);
+    } else {
+      /* Will set bpage to the now cleared page */
+      if (LRU_ClearLast(lru_head, hash_table, bq, &bpage) == -1) {
         return BFE_INCOMPLETEWRITE;
       }
     }
 
-    HT_Remove(hash_table, bpage->fd, bpage->pagenum);
-
-    /* Remove from the end of LRU */
-    bpage->prevpage->nextpage = NULL;
-    bpage->prevpage = NULL;
-
-    /*
-     * Now initialize it anew and put it at the front of LRU
-     * and put it in the hash table
-     */
     if (initialize_bpage(bq, bpage) == -1) {
       return BFE_INCOMPLETEREAD;
     }
 
-    bpage->nextpage = lru_head;
-    lru_head->prevpage = bpage;
-
+    LRU_Push(&lru_head, bpage);
     HT_Add(hash_table, bpage);
   }
 
+  *fpage = &bpage->fpage;
+  return BFE_OK;
+}
+
+int BF_AllocBuf(BFreq bq, PFpage **fpage) {
+  BFpage *bpage;
+
+  bpage = HT_Find(hash_table, bq.fd, bq.pagenum);
+  /* Since we're allocating a new block, there shouldn't exist one currently */
+  if (bpage) {
+    return BFE_PAGEINBUF;
+  }
+
+  if (free_list_head) {
+    bpage = FL_Pop(&free_list_head);
+  } else {
+    if (LRU_ClearLast(lru_head, hash_table, bq, &bpage) == -1) {
+      return BFE_INCOMPLETEWRITE;
+    }
+  }
+
+  /* Because it's a new page we shouldn't read it */
+  initialize_bpage_no_read(bq, bpage);
+
+  LRU_Push(&lru_head, bpage);
+  HT_Add(hash_table, bpage);
+
+  *fpage = &bpage->fpage;
   return BFE_OK;
 }
