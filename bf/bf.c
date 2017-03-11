08c41a8b451599ec1b876ac21b95d0e3d1f69751
Simon Kindström <simon.kindstrom@gmail.com>
2017-03-11 13:33:13 +0900

    Finished BF_GetBuf

--- bf/bf.c
+++ bf/bf.c
@@ -54,11 +54,14 @@ void BF_Init(void) {
   hash_table = HT_Init(BF_HASH_TBL_SIZE);
 }
 
+/* Returns -1 on read error */
 int initialize_bpage(BFreq bq, BFpage *page) {
   page->dirty = FALSE;
   page->count = 1;
   page->fd = bq.fd;
   page->pagenum = bq.pagenum;
+  page->nextpage = NULL;
+  page->prevpage = NULL;
 
   /* Read page data from an offset. It assumes that pagenum is zero-indexed */
   lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
@@ -73,25 +76,64 @@ int BF_GetBuf(BFreq bq, PFpage **fpage) {
   if (bpage) {
     ++bpage->count;
     *fpage = &bpage->fpage;
+  } else if (free_list_head) {
+    /* if there is available space in FreeList just use that */
 
-    return BFE_OK;
-  }
-
-  /* if there is available space in FreeList just use that */
-  if (free_list_head) {
     /* Pop an element from the free_list */
     bpage = free_list_head;
     free_list_head = bpage->nextpage;
 
-    initialize_bpage(bq, bpage);
+    if (initialize_bpage(bq, bpage) == -1) {
+      return BFE_INCOMPLETEREAD;
+    }
     /* Update LRU. The least recently used element is at the front */
     bpage->nextpage = lru_head;
     lru_head->prevpage = bpage;
 
     HT_Add(hash_table, bpage);
+  } else {
+    /*
+     * If it's not in the hash table and there's no space in the free list,
+     * We have to kick a element from the LRU. Also make sure to remove
+     * it from the hash table
+     */
 
-    return BFE_OK;
+    bpage = lru_head;
+    /*
+     * Find the least recently used object (the tail)
+     * TODO: Keep a pointer to the tail to not have to go through the whole list
+     */
+    while (bpage->nextpage) {
+      bpage = bpage->nextpage;
+    }
+
+    /* Only have to write if the page is dirty */
+    if (bpage->dirty) {
+      lseek(bq.unixfd, (PAGE_SIZE * bq.pagenum), SEEK_SET);
+      if (write(bq.unixfd, bpage->fpage.pagebuf, PAGE_SIZE) == -1) {
+        return BFE_INCOMPLETEWRITE;
+      }
+    }
+
+    HT_Remove(hash_table, bpage->fd, bpage->pagenum);
+
+    /* Remove from the end of LRU */
+    bpage->prevpage->nextpage = NULL;
+    bpage->prevpage = NULL;
+
+    /*
+     * Now initialize it anew and put it at the front of LRU
+     * and put it in the hash table
+     */
+    if (initialize_bpage(bq, bpage) == -1) {
+      return BFE_INCOMPLETEREAD;
+    }
+
+    bpage->nextpage = lru_head;
+    lru_head->prevpage = bpage;
+
+    HT_Add(hash_table, bpage);
   }
 
-  return BFE_NOBUF;
+  return BFE_OK;
 }
