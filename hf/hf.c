6955189ee20c842a62bd5fc5e222e96a0f680921
Taylor Soulis <taylorsoulis@Taylors-MacBook-Pro.local>
2017-04-23 16:52:51 +0900

    Added hf.c file

---
+++ hf/hf.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "minirel.h"
+
+#include "bf.h"
+#include "pf.h"
+#include "hf.h"
